d1f9eace994731f01a3e5fdb3ff912910013a0b1
Claudia Robinson <claudiamegr@gmail.com>
2020-06-10 15:31:18 -0400

    Added search functionality and JSDoc commenting

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 let listItems = document.getElementsByClassName("student-item cf");
 let itemsPerPage = 10;
+let pageDiv = document.getElementsByClassName("page")[0];
+let searchDiv = document.createElement("div");
+searchDiv.className = "student-search";
+let searchBar = document.createElement("input");
+searchBar.placeholder = "Search for students...";
+let searchButton = document.createElement("button");
+searchButton.textContent = "Search";
+searchDiv.appendChild(searchButton);
+searchDiv.insertBefore(searchBar, searchButton);
+let headerDiv = document.getElementsByClassName("page-header cf")[0];
+headerDiv.appendChild(searchDiv);
+
+/**
+ * showPage determines which objects will be visible on a given page provided the page number and an array of objects.
+ *
+ * @param {array} list - the given array of student objects that is paginated
+ * @param {number} page - the given page number
+ */
 
 function showPage(list, page) {
   let startIndex = page * itemsPerPage - itemsPerPage;
@@ -13,21 +31,13 @@ function showPage(list, page) {
   }
 }
 
-function createSearch() {
-  let searchDiv = document.createElement("div");
-  searchDiv.className = "student-search";
-  let searchBar = document.createElement("input");
-  searchBar.placeholder = "Search for students...";
-  let searchButton = document.createElement("button");
-  searchButton.textContent = "Search";
-  searchDiv.appendChild(searchButton);
-  searchDiv.insertBefore(searchBar, searchButton);
-  let headerDiv = document.getElementsByClassName("page-header cf")[0];
-  headerDiv.appendChild(searchDiv);
-}
+/**
+ * appendPages creates links to as many pages are necessary to display the objects of a given array across multiple pages, with no more than a certain number of objects per page.
+ *
+ * @param {array} list - the given array of student objects that is paginated
+ */
 
 function appendPages(list) {
-  let pageDiv = document.getElementsByClassName("page")[0];
   let pageNumbersDiv = document.createElement("div");
   pageNumbersDiv.className = "pagination";
   let ul = document.createElement("ul");
@@ -66,6 +76,43 @@ function appendPages(list) {
   }
 }
 
-createSearch();
+/**
+ * searchFunctionality displays results of student objects that include a given string provided by user in the searchbar. If no results are found, an error message is displayed.
+ */
+
+function searchFunctionality() {
+  let searchInput = searchBar.value;
+  let resultsFound = [];
+  for (let i = 0; i < listItems.length; i += 1) {
+    listItems[i].style.display = "none";
+    if (
+      searchInput.length !== 0 &&
+      listItems[i].textContent.toLowerCase().includes(searchInput.toLowerCase())
+    ) {
+      listItems[i].style.display = "block";
+      resultsFound.push(listItems[i]);
+    } else if (searchInput.length === 0) {
+      resultsFound.push(listItems[i]);
+    }
+  }
+  let message = document.getElementById("sorry");
+  if (message) {
+    pageDiv.removeChild(message);
+  }
+  if (resultsFound.length === 0) {
+    let notFoundMessage = document.createElement("p");
+    notFoundMessage.textContent = "Sorry, no results found.";
+    notFoundMessage.id = "sorry";
+    pageDiv.appendChild(notFoundMessage);
+  }
+  pageDiv.removeChild(document.getElementsByClassName("pagination")[0]);
+  showPage(resultsFound, 1);
+  appendPages(resultsFound);
+}
+
+searchBar.addEventListener("keyup", searchFunctionality);
+
+searchButton.addEventListener("submit", searchFunctionality);
+
 showPage(listItems, 1);
 appendPages(listItems);
